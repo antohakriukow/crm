25929d08e3ae6909354bedea4d83d382b3ed9d21
Anton Kryukov <antohakriukow@gmail.com>
2022-06-29 13:41:07 +0300

    technical commit

--- src/components/ui/Logo/Logo.tsx
+++ src/components/ui/Logo/Logo.tsx
@@ -5,7 +5,7 @@ import styles from './Logo.module.scss'
 const Logo: FC = () => {
 	return (
 		<div className={styles.logo}>
-			<p className={styles.logo__white}>Bitrix</p>
+			<p className={styles.logo__white}>Pitrix</p>
 			<p className={styles.logo__blue}>24</p>
 		</div>
 	)
